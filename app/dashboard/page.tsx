0c18305bcd25c1ab68c90170ae85aba9f823d0e3
youssefzeed1 <youssefziedhemeda@gmail.com>
2026-05-13 21:15:03 +0300

    Flast

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabase";
 import Link from "next/link";
 
+
 export default function Dashboard() {
   const [briefs, setBriefs] = useState<any[]>([]);
 
